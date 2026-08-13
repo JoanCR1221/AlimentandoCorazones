6f8a6576df060c6aba7e1e8adbe78679af2833d6
PabloD6003 <pablodavidgomezv@gmail.com>
2026-08-13 10:02:26 -0600

    Revertir comentario en BeneficiarioDtos

--- SIGAC/SIGAC.Application/DTOs/BeneficiarioDtos.cs
+++ SIGAC/SIGAC.Application/DTOs/BeneficiarioDtos.cs
@@ -2,7 +2,7 @@
 {
     public class BeneficiarioCrearDto
     {
-       // public int Id { get; set; } esto se genera en BD, no el usuario
+        public int Id { get; set; }
         public string Nombre { get; set; } = string.Empty;
         public DateTime FechaNacimiento { get; set; }
         public string Categoria { get; set; } = string.Empty;
